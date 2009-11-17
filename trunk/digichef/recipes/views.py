bbe2e9a82a6cb1c66e6c0f17d5daffaf4f236e4a
afromonkey0 <afromonkey0@39edf0c0-ba50-11de-869a-473eb4776397>
2009-11-17 15:20:47 +0000

    Added detail and list views for recipes

--- trunk/digichef/recipes/views.py
+++ trunk/digichef/recipes/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+from django.http import HttpResponse
+from models import Recipe
+from django.shortcuts import render_to_response, get_object_or_404
+
+
+def recipes(request):
+	"""View of a list of recipes"""
+	recipe_list = Recipe.objects.all().order_by('title')
+	return render_to_response('recipe_list.html', {'recipe_list' : recipe_list})
+
+
+def recipe_detail(request, recipe_id):
+	"""View for a detailed look at a single recipe
+
+		returns 404 error is recipe not found"""
+	recipe = get_object_or_404(Recipe, pk=recipe_id)
+	ingredient_list = recipe.ingredients.split("\n")
+	return render_to_response('recipe_detail.html', {'recipe' : recipe, 'ingredient_list' : ingredient_list})
