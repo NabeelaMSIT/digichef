24d25858760482bfa6f8a3970c39853a8d77c519
gerrard8dg <gerrard8dg@39edf0c0-ba50-11de-869a-473eb4776397>
2009-11-17 14:14:01 +0000

    added recipes to the models.py

--- trunk/digichef/recipes/models.py
+++ trunk/digichef/recipes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    title= models.CharField(max_length=200)
+    ingredients = models.TextField()
+    ingredients = models.TextField()
+    def __unicode__(self):
+        return self.title
+        
+
