b33f4a5ac97548d3e284456d6d2d89df47bc65f9
afromonkey0 <afromonkey0@39edf0c0-ba50-11de-869a-473eb4776397>
2009-11-17 14:24:32 +0000

    Adding tagging property of Recipes, and changed whitespace

--- digichef/recipes/models.py
+++ digichef/recipes/models.py
@@ -1,11 +1,22 @@
 from django.db import models
+from tagging.models import Tag
+
 
 # Create your models here.
 class Recipe(models.Model):
-    title= models.CharField(max_length=200)
-    ingredients = models.TextField()
-    ingredients = models.TextField()
-    def __unicode__(self):
-        return self.title
+	title = models.CharField(max_length=200)
+	ingredients = models.TextField()
+	instructions = models.TextField()
+	
+	def _get_tags(self):
+		return Tag.objects.get_for_object(self)
+
+	def _set_tags(self, tag_list):
+		Tag.objects.update_tags(self, tag_list)
+
+	tags = property(_get_tags, _set_tags)
+
+	def __unicode__(self):
+		return self.title
         
 
