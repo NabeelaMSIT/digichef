c3cabbde5f13d6ba2a2822e9fa27900e66267ec2
afromonkey0 <afromonkey0@39edf0c0-ba50-11de-869a-473eb4776397>
2009-11-19 11:11:13 +0000

    Removed debug statements and commented remakeDatabase.py

--- trunk/digichef/remakeDatabase.py
+++ trunk/digichef/remakeDatabase.py
@@ -4,19 +4,16 @@ import os
 
 PATH = os.path.abspath(os.path.dirname(__file__))
 
+# Make sure the user understands what's up
 print """This will delete all data in the database
 AND REPLACE IT WITH DEFAULT DATA!!!"
 THE ONLY DATA LEFT WILL BE THAT SPECIFIED IN loaddata.py"
 You lose anything else added later"
 It should be noted that this isn't a joke"""
 reply = raw_input("Are you seriously sure (type 'yes' to continue)? ")
+#if they are sure
 if reply == 'yes':
-	print "here"
-	os.system("python manage.py reset recipes tagging --noinput")
-	print "here2"
+	os.system("python manage.py reset recipes tagging --noinput")# wipe DB
 	ppath = MEDIA_ROOT = os.path.join(PATH, '..')
-	print ppath
-	print "here3"
-	os.system("PYTHONPATH=\"%s\" DJANGO_SETTINGS_MODULE=digichef.settings python loaddata.py" % ppath)
-	print "here4"
+	os.system("PYTHONPATH=\"%s\" DJANGO_SETTINGS_MODULE=digichef.settings python loaddata.py" % ppath)# fill with default data
 
