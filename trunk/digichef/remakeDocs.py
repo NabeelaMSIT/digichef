2960288cde805df961dd1d9f9c2cf740aaad8401
afromonkey0 <afromonkey0@39edf0c0-ba50-11de-869a-473eb4776397>
2009-12-02 13:48:02 +0000

    Rearranged docs directory structure

--- trunk/digichef/remakeDocs.py
+++ trunk/digichef/remakeDocs.py
@@ -5,7 +5,7 @@ import os
 PATH = os.path.abspath(os.path.dirname(__file__))
 
 ppath = os.path.join(PATH, '..')
-docpath = os.path.join(PATH, 'docs')
+docpath = os.path.join(PATH, 'docs/html')
 
 # big 'ol command line to run epydoc with all the right settings. Won't work on windows but I don't care.
 os.system("PYTHONPATH=\"%s\" DJANGO_SETTINGS_MODULE=digichef.settings epydoc --html --parse-only --docformat plaintext re -v -o \"%s\" --graph all digichef" % (ppath, docpath))
