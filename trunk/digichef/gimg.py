c112b4d9cc0f9e58b7b12d3f83ce7f58069c6e96
RobertSKMiles <RobertSKMiles@39edf0c0-ba50-11de-869a-473eb4776397>
2010-03-24 15:00:50 +0000

    fixed repos problems hopefully NOTE INDEX PAGE BROKEN

--- trunk/digichef/gimg.py
+++ trunk/digichef/gimg.py
@@ -5,8 +5,9 @@ from time import sleep
 
 import os
 
+
 for recipe in Recipe.objects.all():
-	img_url = os.popen("""wget "http://images.google.com/images?q=%s" -U Mozilla -q -O - | tidy -q 2>/dev/null | grep -i -m1 imgurl | egrep -o "http://[^&]+" | head -n1""" % recipe.title).read()
+	img_url = os.popen("""wget "http://images.google.com/images?q=%s&imgsz=m" -U Mozilla -q -O - | tidy -q 2>/dev/null | grep -i -m1 imgurl | egrep -o "http://[^&]+" | head -n1""" % recipe.title).read()
 	print recipe.title,":",img_url[:40]
 	recipe.image_url = img_url
 	recipe.save()
